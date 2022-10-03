bc4cfbdd32ab570ffc33ddf3afc72117128b7de2
Aleksander Radovsky <alrad.sys@gmail.com>
2022-10-03 21:32:59 +0300

    add env variable `CALC_COMPLEX`

--- hw_1_4/server.py
+++ hw_1_4/server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 
 from hw_1_4.app.service import Blockchain
@@ -5,7 +7,7 @@ from hw_1_4.app.handlers import Handler
 
 if __name__ == "__main__":
     app = Flask(__name__)
-    blockchain = Blockchain()
+    blockchain = Blockchain(calc_complex=os.getenv('CALC_COMPLEX', '00000'))
     handler = Handler(blockchain)
     handler.init_routes(app)
     app.run(host="127.0.0.1", debug=True, port=5000)
