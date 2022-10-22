e1c87ac32aa9e03ca532f01f143695e3745bbc80
Aleksander Radovsky <alrad.sys@gmail.com>
2022-10-22 16:23:58 +0300

    add tests

--- hw_1_6/tests/test_server.py
+++ hw_1_6/tests/test_server.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 import time
@@ -8,7 +9,17 @@ import subprocess
 from server import TaskQueueServer
 
 
-class ServerBaseTest(TestCase):
+class ServerTestCase(TestCase):
+    def send(self, command):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("127.0.0.1", 5432))
+        s.send(command)
+        data = s.recv(1000000)
+        s.close()
+        return data
+
+
+class ServerBaseTest(ServerTestCase):
     def setUp(self):
         self.server = subprocess.Popen(["python3", "server.py"])
         # даем серверу время на запуск
@@ -17,14 +28,6 @@ class ServerBaseTest(TestCase):
     def tearDown(self):
         self.server.terminate()
         self.server.wait()
-
-    def send(self, command):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("127.0.0.1", 5432))
-        s.send(command)
-        data = s.recv(1000000)
-        s.close()
-        return data
 
     def test_base_scenario(self):
         task_id = self.send(b"ADD 1 5 12345")
@@ -60,6 +63,69 @@ class ServerBaseTest(TestCase):
 
     def test_wrong_command(self):
         self.assertEqual(b"ERROR", self.send(b"ADDD 1 5 12345"))
+
+    def test_wrong_task_id(self):
+        self.assertEqual(b"NO", self.send(b"IN 1 12345"))
+        self.assertEqual(b"NO", self.send(b"ACK 1 12345"))
+        self.assertEqual(b"NONE", self.send(b"GET 1 12345"))
+
+    def test_wrong_queue_id(self):
+        self.assertEqual(b"NONE", self.send(b"GET UNDEF 1"))
+
+
+class ServerTimeoutTest(ServerTestCase):
+    def setUp(self):
+        self.server = subprocess.Popen(["python3", "server.py", "-t", "2"])
+        # даем серверу время на запуск
+        time.sleep(0.5)
+
+    def tearDown(self):
+        self.server.terminate()
+        self.server.wait()
+
+    def test_timeout(self):
+        task_id = self.send(b"ADD 1 5 12345")
+        self.send(b"GET 1 " + task_id)
+        self.assertEqual(b"YES", self.send(b"IN 1 " + task_id))
+        time.sleep(1)
+        self.assertEqual(b"NONE", self.send(b"GET 1"))
+        time.sleep(2)
+        self.assertEqual(task_id + b" 5 12345", self.send(b"GET 1"))
+
+
+class ServerSaveTest(TestCase):
+    def _start_server(self):
+        self.server = subprocess.Popen(["python3", "server.py", "-c", "./"])
+        # даем серверу время на запуск
+        time.sleep(0.5)
+
+    def setUp(self):
+        self._start_server()
+
+    def tearDown(self):
+        self.server.terminate()
+        self.server.wait()
+        os.remove("./tasks")
+
+    def send(self, command):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("127.0.0.1", 5432))
+        s.send(command)
+        data = s.recv(1000000)
+        s.close()
+        return data
+
+    def test_save(self):
+        task_id = self.send(b"ADD 1 5 12345")
+        self.assertEqual(b"YES", self.send(b"IN 1 " + task_id))
+        self.send(b"SAVE")
+        self.server.terminate()
+        self.server.wait()
+        self.server = subprocess.Popen(["python3", "server.py", "-c", "./"])
+        # даем серверу время на запуск
+        time.sleep(0.5)
+        self.assertEqual(b"YES", self.send(b"IN 1 " + task_id))
+        self.assertEqual(task_id + b" 5 12345", self.send(b"GET 1"))
 
 
 if __name__ == "__main__":
